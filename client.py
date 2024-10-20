79c88ef20be7985ca8fe84e33659bfacc10755fb
Ritik Gupta <gritik95@gmail.com>
2024-10-20 23:14:30 +0530

    Update code to allow continuos feed

--- client.py
+++ client.py
@@ -1,14 +1,25 @@
 import socket
 
+HEADER_SIZE = 10 # Similar headersize as server
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
 
-# Basic functionality to get full message, using a small buffer size.
-full_msg = ''
 while True:
-    msg = s.recv(1) # Very small buffer size
-    if len(msg) <= 0:
-        break
-    full_msg += msg.decode("utf-8")
+    # Basic functionality to get full message, using a small buffer size.
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(HEADER_SIZE)
+        if new_msg:
+            print(f"New Message Length: {msg[:HEADER_SIZE]}") 
+            msglen = int(msg[:HEADER_SIZE])
+            new_msg = False
+            print(msg.decode('utf-8'))
+        full_msg += msg.decode('utf-8')
 
-print(full_msg)
+        if len(full_msg)-HEADER_SIZE == msglen:
+            print("Full Message Received")
+            print(full_msg[HEADER_SIZE:])
+            new_msg = True
+            full_msg = ''
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 import socket
 
+# We use headers to inform client about the size of data etc
+HEADER_SIZE = 10
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
 s.listen(5)
@@ -7,6 +10,7 @@ s.listen(5)
 while True:
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
-    clientsocket.close()
-    
+    msg = "Welcome to the server!"
+    msg = f'{len(msg):<{HEADER_SIZE}}' + msg
+    clientsocket.send(bytes(msg, "utf-8"))
+    # clientsocket.close()
